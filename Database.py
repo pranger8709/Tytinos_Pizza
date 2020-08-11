ba6f74ff7fb73112419194c046fb9b8d40812b53
jcondroid <jconway2@live.maryville.edu>
2020-08-10 23:05:30 -0600

    Database class no work

--- Database.py
+++ Database.py
@@ -1,38 +1,12 @@
+"""
+I may scrap this idea :/
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 
-# GLOBALS
-Base = declarative_base()
-debug = False
-
-
-class Coupon(Base):
-    def __init__(self, type, discount):
-        self.type = type
-        self.discount = discount
-
-    __tablename__ = 'coupon'
-
-    id = Column(Integer, primary_key=True)
-    type = Column(String)
-    discount = Column(Integer)
-
-    def get_discount(self):
-        return self.discount / 100
-
-    """ TODO: MOVE. I believe this will go into the Cart or Menu class. """
-    def apply_discount(self):
-        return self._coupon_type.apply_discount(self._order_total)
-
-    def __repr__(self):
-        return "Type: {}, Discount: {}%".format(self.type, self.discount)
-
-
-# Use SQLAlchemy to store and retrieve objects from the internal SQLite database
-class Database(Coupon):
+class Database:
     def __init__(self):
+        Base = declarative_base()
         self.db_engine = create_engine('sqlite:///:memory:', echo=False)
         Base.metadata.create_all(self.db_engine)
         self.Session = sessionmaker(bind=self.db_engine)
@@ -69,3 +43,4 @@ if __name__ == '__main__':
     db.add_data("Refer Friend", 25)
     db.add_data("Monthly Special", 10)
     db.view_data()
+"""
