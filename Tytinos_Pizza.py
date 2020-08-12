d6bdb23542c8d18117936f4ee307daccf50954b2
tylerpranger <tylerpranger@gmail.com>
2020-08-12 12:37:56 -0600

    added more stability to customer class

--- Tytinos_Pizza.py
+++ Tytinos_Pizza.py
@@ -4,6 +4,5 @@ from Person import * #Added standard users for Justin, April and Tyler
 
 def main():
     customer = Customer()
-    customer.add_customer('Some', 'Person', 'password!')
-    customer.print_customer()
+    
 main()
