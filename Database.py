4b3779ea528d98e383e20b26eadeac4c11a23a3c
jcondroid <jconway2@live.maryville.edu>
2020-08-12 21:54:16 -0600

    Coupon using strategy

--- Database.py
+++ Database.py
@@ -1,8 +1,7 @@
-"""
-I may scrap this idea :/
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 
 class Database:
     def __init__(self):
@@ -15,32 +14,19 @@ class Database:
     def teardown(self):
         self.session.remove()
 
-    def test(self):
-        self.add_data("New User", 20)
-        self.add_data("Weekly Special", 15)
-        self.add_data("Refer Friend", 25)
-
-        # Test if data appears
-        assert self.session.query(Coupon).filter(Coupon.discount == 20).first().get_discount() == 0.2
-        assert self.session.query(Coupon).filter(Coupon.discount == 15).first().get_discount() == 0.15
-        assert self.session.query(Coupon).filter(Coupon.discount == 25).first().get_discount() == 0.25
-
-    def add_data(self, coupon_type, coupon_discount):
-        self.session.add(Coupon(type=coupon_type, discount=coupon_discount))
+    def add_data(self, data_object):
+        self.session.add(data_object)
         self.session.commit()
 
-    def view_data(self):
-        for instance in self.session.query(Coupon).order_by(Coupon.id):
+    def view_data(self, object):
+        for instance in self.session.query(object).order_by(object.id):
             print("{}: {}".format(instance.type, instance.discount))
 
 
-if __name__ == '__main__':
-    db = Database()
-    if debug:
-        db.test()
-    db.add_data("New User", 20)
-    db.add_data("Weekly Special", 15)
-    db.add_data("Refer Friend", 25)
-    db.add_data("Monthly Special", 10)
-    db.view_data()
-"""
+# if __name__ == '__main__':
+#     db = Database()
+#     db.add_data("New User", 20)
+#     db.add_data("Weekly Special", 15)
+#     db.add_data("Refer Friend", 25)
+#     db.add_data("Monthly Special", 10)
+#     db.view_data()
