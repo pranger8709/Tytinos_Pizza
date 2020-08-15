5cbdcd0de2af75f23b55b91d71468826d456931f
jcondroid <jconway2@live.maryville.edu>
2020-08-14 22:32:49 -0600

    Small modification to Cart

--- Cart.py
+++ Cart.py
@@ -8,21 +8,27 @@ Base = declarative_base()
 
 class Cart(Base):
     """ A cart class to keep track of a customer's items. """
-    def __init__(self, item=[], price=None):
-        self._item = item
-        self._price = price
+    def __init__(self, item=[], price=[]):
+        self.item = item
+        self.price = price
+		self._total_price = 0
 
-    def add_item_to_cart(self, new_item):
-        self._item.append(new_item)
-
-    def get_item_count(self):
-        return len(self._item)
+    def add_item_to_cart(self, item_object):
+        self.item.append(item_object.item)
+		self.price.append(item_object.price)
 
     def remove_item_from_cart(self, item_id):
         print("Remove item from cart")
 
+    def get_item_count(self):
+        return len(self._item)
+		
+	def get_price(self):
+		for price in self.price:
+			self._total_price += price
+
     def print_cart(self):
-        for item in self._item:
+        for item in self.item:
             print("""**********\n{}""".format(item))
         print("""**********\n""")
 
