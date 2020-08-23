33a9474d070646ee88e76d970e9d4fc1b1d61008
jcondroid <jconway2@live.maryville.edu>
2020-08-23 01:25:58 -0600

    App portion

--- Tytinos_Pizza.py
+++ Tytinos_Pizza.py
@@ -5,58 +5,39 @@ from Cart import *
 from Delivery import *
 
 
+# Python application using the 6+ major classes demonstrating the core functionality - GUIs are NOT requiredelivery.
 def main():
+    # 6+ Major Classes
     customer = Customer()
+    employee = Employee()
+    justin_cart = Cart()
+    menu = [Pizza(), Side(), Dessert()]
+    delivery = Delivery()
+    coupon = Coupon()
+
+    # Demo Customer Class
     customer.Print_Person()
     customer.Remove_Person(2)
-    customer.Add_Person("Goodbye", "Hello", "test")
-    print("")
-    customer.Print_Person()
-    customer.Remove_Person(1)
-    print("")
+    customer.Remove_Person(3)
+    print("\n########As you can see, Customer's 2 and 3 were deleted:")
     customer.Print_Person()
     
-    employee = Employee()
+    # Demo Employee Class
+    print("\n########Add 2 Test Employees \"Test_Two\" and \"Test_Three\" and then remove them:")
     employee.Add_Person("Test_Two", "Manager", "H@ll@")
     employee.Add_Person("Test_Three", "Manager", "H@ll@")
     employee.Print_Person()
     employee.Remove_Person(4)
     employee.Print_Person()
-    
-    pizza = Pizza()
-    pizza.Print_Pizza_Menu()
-    print("")
-    pizza.Add_Pizza("Meat Sauce", 30.00, 35.00, 40.00)
-    pizza.Print_Pizza_Menu()
-    pizza.Remove_Item("Meat Sauce")
-    pizza.Print_Pizza_Menu()
-    
-    side = Side()
-    side.Print_Side_Menu()
-    print("")
-    side.Add_Side("Sweet Sticks", 30.00, 35.00, 40.00)
-    side.Print_Side_Menu()
-    side.Remove_Item("Sweet Sticks")
-    side.Print_Side_Menu()
-    
-    dessert = Dessert()
-    dessert.Print_Dessert_Menu()
-    print("")
-    dessert.Add_Dessert("Sticks", 30.00)
-    dessert.Print_Dessert_Menu()
-    dessert.Remove_Item("Sticks")
-    dessert.Print_Dessert_Menu()
-    
-    # Justin Cart
-    justin_cart = Cart()
-    menu = [Pizza(), Side(), Dessert()]
 
-    print("\n\n")
+    print("\n########Here is our final Customer List:")
     customer = Customer()
     customer.Print_Person()
+
+    # Declare a customer object. We will modify their account
     justin = customer.session.query(Person).filter(Person.id == 1)[0]
 
-    justin_cart.print_cart(justin)
+    # Demo the Cart class
     # Adding 1 Cheese Pizza Small to Justin's cart
     justin_cart.add_item_to_cart(
         menu[0].session.query(Menu).filter(Menu.itemType == 1).filter(Menu.active == 1).order_by(Menu.id)[0], 
@@ -64,7 +45,7 @@ def main():
         "Small", 
         1
     )
-    justin_cart.print_cart(justin)
+    
     # Adding 2 Meat Lover's Pizzas Large to Justin's cart
     justin_cart.print_price_without_tax()
     justin_cart.add_item_to_cart(
@@ -73,7 +54,10 @@ def main():
         "Large", 
         2
     )
+
+    print("\n########Here is Justin's Cart:")
     justin_cart.print_cart(justin)
+    
     # Removing an item that doesn't exist
     justin_cart.remove_item_from_cart(
         menu[0].session.query(Menu).filter(Menu.itemType == 1).filter(Menu.active == 1).order_by(Menu.id)[0], 
@@ -81,7 +65,7 @@ def main():
         "Veggie",
         5.99
     )
-    justin_cart.print_cart(justin)
+
     # Removing an item that does exist
     justin_cart.remove_item_from_cart(
         menu[0].session.query(Menu).filter(Menu.itemType == 1).filter(Menu.active == 1).order_by(Menu.id)[0], 
@@ -89,7 +73,7 @@ def main():
         "Cheese",
         5.99
     )
-    justin_cart.print_cart(justin)
+    
     # Removing an item that does exist but the price does not match the item (Does not remove item)
     justin_cart.remove_item_from_cart(
         menu[0].session.query(Menu).filter(Menu.itemType == 1).filter(Menu.active == 1).order_by(Menu.id)[0], 
@@ -97,22 +81,24 @@ def main():
         "Meat Lovers",
         5.99
     )
-    justin_cart.print_cart(justin)
-    d = Delivery()
-    d.add_standard_delivery_types()
-    print(d.does_delivery_type_exist("Delivery", 3.99))
-    print(d.does_delivery_type_exist("Pick up", 0.00))
-    print(d.does_delivery_type_exist("Door Dash", 3.99))
-    d.print_delivery_types()
-    d.remove_delivery_type("Door Dash", 3.99)
-    d.print_delivery_types()
-    selected_delivery_type = d.get_delivery_price_from_deliver_type_name("Delivery")
+    
+    # Demo the Delivery class
+    delivery.add_standard_delivery_types()
+
+    print("\n########Here are all of the standard Delivery types:")
+    delivery.print_delivery_types()
+    delivery.remove_delivery_type("Door Dash", 3.99)
+
+    # Declare a delivery type
+    selected_delivery_type = delivery.get_delivery_price_from_delivery_type_name("Delivery")
+
+    print("\n########Here we apple the Delivery charge to the total price in our cart:")
     print(selected_delivery_type)
     justin_cart.add_item_price_to_total_price(selected_delivery_type)
     justin_cart.print_cart(justin)
 
-    # Coupon Database ORM
-    coupon = Coupon()
+    # Demo Coupon
+    print("\n########Here are our standard coupons with their discounts:")
     coupon.add_standard_coupon_types()
     coupon.print_coupon_table()
 
@@ -120,11 +106,9 @@ def main():
     new_user_coupon = NewUserCoupon()
     weekly_special_coupon = WeeklySpecialCoupon()
     refer_friend_coupon = ReferFriendCoupon()
-    print(new_user_coupon.get_discount_amount(coupon))
-    print(weekly_special_coupon.get_discount_amount(coupon))
-    print(refer_friend_coupon.get_discount_amount(coupon))
 
     # Apply a coupon to a customer
+    print("\n########Here we apply a coupon to Justin's Cart:")
     print("\n\n*******************************************\nNo Discount:")
     justin_cart.print_cart(justin)
     coupon.print_coupon_table()
@@ -135,6 +119,7 @@ def main():
     print("*******************************************\n")
 
     # Check if coupon has been applied
+    print("\n########Here we check to see if our Coupon has been applied:")
     print("Did Justin apply New User coupon? {}".format(new_user_coupon.check_if_person_id_has_used_coupon(coupon, justin)))
     print("Did Justin apply Refer Friend coupon? {}".format(refer_friend_coupon.check_if_person_id_has_used_coupon(coupon, justin)))
     
